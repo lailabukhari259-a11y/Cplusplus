9915c7a47ec0130d8e5f6e36296419fb74facba5
lailabukhari259-a11y <Lailabukhari@gmail.com>
2025-10-27 10:47:40 +0500

    print name register

---
+++ labassignment1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Name: Laila Aslam" << endl;
+    cout << "Registration Number: 23182" << endl;
+    cout << "Course Name: Software Engineering" << endl;
+
+    return 0;
+}
+
